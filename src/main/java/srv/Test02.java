886eeea4ba360224f82a4f69be699c1021eac913
hokke <oota.123@gmail.com>
2018-12-12 07:31:18 +0900

    idea追加

---
+++ src/main/java/srv/Test02.java
@@ -0,0 +1,24 @@
+package srv;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Test02", urlPatterns = "/Test02")
+public class Test02 extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//         pr = res.getWriter();
+//        res.setContentType("text/html");
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pr = res.getWriter();
+        pr.println("サンプル");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,9 @@
 <html>
 gradle-tomcat-plugin test<br />
 update
+
+<ul>
+    <li><a href="Test02">Test02</a></li>
+    <li><a href="Test01">Test01</a></li>
+</ul>
 </html>
